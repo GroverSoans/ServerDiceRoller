f1f5a23f9d9247bfae360c8c5ab147882ca3724b
Grover Soans <groverpsoans@gmail.com>
2024-10-18 14:42:40 -0500

    added html test page

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ app.use(cors({
     origin: 'https://delightful-ocean-0e31ab010.5.azurestaticapps.net'  // Replace with your static web app's URL
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html on the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-
 
 
 app.get('/api/rollDice', (req, res) => {
